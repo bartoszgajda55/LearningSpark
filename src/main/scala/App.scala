6cb9efe6886c192892d3db3018d897a82d189a5a
Bartosz Gajda <bg@bartoszgajda.com>
2019-07-09 20:16:32 +0200

    Fix logging levels of Spark.

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,7 +1,11 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
 
 object App {
   def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    Logger.getLogger("akka").setLevel(Level.ERROR)
     val spark = SparkSession
       .builder()
       .appName("Spark First Example")
