df62c21b1b565cae4b9c572263572d8a66872b6b
Bartosz Gajda <bg@bartoszgajda.com>
2019-07-10 17:49:57 +0200

    Start coding user defined aggregate function.

---
+++ src/main/scala/UserDefinedAggregate.scala
@@ -0,0 +1,13 @@
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.expressions.MutableAggregationBuffer
+import org.apache.spark.sql.expressions.UserDefinedAggregateFunction
+import org.apache.spark.sql.types._
+
+object UserDefinedAggregate extends UserDefinedAggregateFunction {
+  // Data types of input arguments of this aggregate function
+  def inputSchema: StructType = StructType(StructField("inputColumn", LongType) :: Nil)
+  // Data types of values in their aggregation buffer
+  def bufferSchema: StructType = {
+    StructType(StructField("sum", LongType) :: StructField("count", LongType) :: Nil)
+  }
+}
