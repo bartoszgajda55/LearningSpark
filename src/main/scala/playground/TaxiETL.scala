9d9400b34875e0e04250d945b3612fbc9428a01e
Bartosz Gajda <bartek.g55@gmail.com>
2020-09-01 16:52:03 +0200

    Add lacking system exit when no params supplied.

--- src/main/scala/playground/TaxiETL.scala
+++ src/main/scala/playground/TaxiETL.scala
@@ -6,23 +6,24 @@ import org.apache.spark.sql.functions._
 object TaxiETL {
   def main(args: Array[String]): Unit = {
 
-//    if(args.length != 2) { // prod version only
-//      println("Need two arguments: 1) input data path 2) output data path")
-//    }
+    if(args.length != 2) { // prod version only
+      println("Need two arguments: 1) input data path 2) output data path")
+      sys.exit(-1)
+    }
 
     val spark = SparkSession.builder()
       .appName("Taxi ETL")
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-      .master("local[1]") // dev version only
+//      .master("local[1]") // dev version only
       .getOrCreate()
 
-    val devDataPath: String = "data/taxi/raw/green_tripdata_2019-01.csv"
+//    val devDataPath: String = "data/taxi/raw/green_tripdata_2019-01.csv"
 
     val df = spark.read
       .option("header", "true")
       .option("inferSchema", "true")
-      .csv(devDataPath) // dev
-//      .csv(args(0)) // prod
+//      .csv(devDataPath) // dev
+      .csv(args(0)) // prod
 
     val dfWithDateColumns = df
       .withColumn("year", year(col("lpep_pickup_datetime")))
@@ -36,7 +37,7 @@ object TaxiETL {
       .write
       .partitionBy("year", "month", "day")
       .mode(SaveMode.Overwrite)
-      .csv("data/taxi/transformed") // dev
-//      .csv(args(1)) // prod
+//      .csv("data/taxi/transformed") // dev
+      .csv(args(1)) // prod
   }
 }
